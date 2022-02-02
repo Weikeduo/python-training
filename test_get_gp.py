83cae120ed3c08b8e7495f5984c37b8fcb8b01ae
victo <victorkokoryshkin@gmail.com>
2022-02-02 16:21:03 +0700

    Привел к запускаемому виду тест

--- test_get_gp.py
+++ test_get_gp.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+
 
 class TestGetGp(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
+        self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
 
     def test_get_gp(self):
@@ -27,14 +26,15 @@ class TestGetGp(unittest.TestCase):
         wd.find_element_by_xpath("//a[@id='GpTypesSelector_0']/span").click()
         wd.find_element_by_id("Gps").click()
         wd.find_element_by_xpath("//a[@id='GpsSelector_4']/span").click()
+        wd.find_element_by_id("GpVersions").click()
         wd.find_element_by_xpath("//a[@id='GpVersionsSelector_3']/span").click()
         wd.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='Удалить'])[1]/following::span[2]").click()
-    
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
-    
+
     def is_alert_present(self):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
@@ -42,6 +42,7 @@ class TestGetGp(unittest.TestCase):
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
